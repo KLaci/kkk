bb5ba8010f93d9780b67d185148f9908a566785c
Laszlo Kiss <lkiss@hiya.com>
2020-02-02 21:01:23 +0100

    Sorting

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,43 +1,65 @@
 import React, { useEffect, useState, useContext } from "react";
 import { loadAdminData } from "../utils/dataService";
-import { Input } from "antd";
+import { Input, Table } from "antd";
 import { SelectedDateContext } from "./App";
+import Column from "antd/lib/table/Column";
 
 const AdminRow = ({ data }) => {
-  return (
-    <tr>
-      <td>{data.name}</td>
-      <td>{data.checkinTime && data.checkinTime.toLocaleTimeString("hu-HU")}</td>
-      <td>{data.checkoutTime && data.checkoutTime.toLocaleTimeString("hu-HU")}</td>
-      <td>
-        <Input value={data.comment}></Input>
-      </td>
-    </tr>
-  );
+    return (
+        <tr>
+            <td>{data.name}</td>
+            <td>{data.checkinTime && data.checkinTime.toLocaleDateString("hu-HU")}</td>
+            <td>{data.checkinTime && data.checkinTime.toLocaleTimeString("hu-HU")}</td>
+            <td>{data.checkoutTime && data.checkoutTime.toLocaleTimeString("hu-HU")}</td>
+            <td>
+                <Input value={data.comment}></Input>
+            </td>
+        </tr>
+    );
 };
 
 export default () => {
-  const [adminData, setAdminData] = useState([]);
-  const { selectedMonth } = useContext(SelectedDateContext);
-  useEffect(() => {
-    setAdminData(loadAdminData(selectedMonth));
-  }, [selectedMonth]);
+    const [adminData, setAdminData] = useState([]);
+    const { selectedMonth } = useContext(SelectedDateContext);
+    useEffect(() => {
+        setAdminData(loadAdminData(selectedMonth));
+    }, [selectedMonth]);
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Név</th>
-          <th>Bejelentkezési idő</th>
-          <th>Kijelentkezési idő</th>
-          <th>Megjegyzés</th>
-        </tr>
-      </thead>
-      <tbody>
-        {adminData.map((d, index) => (
-          <AdminRow key={index} data={d}></AdminRow>
-        ))}
-      </tbody>
-    </table>
-  );
+    return (
+        <Table dataSource={adminData} style={{ width: "100%" }}>
+            <Column title="Név" dataIndex="name" key="name" sorter={(a, b) => a.name.localeCompare(b.name)} sortDirections={["descend", "ascend"]} />
+            <Column
+                title="Nap"
+                dataIndex="checkinTime"
+                key="checkinDate"
+                render={time => {
+                    return <span>{time && time.toLocaleDateString("hu-HU")}</span>;
+                }}
+            />
+            <Column
+                title="Bejelentkezési idő"
+                dataIndex="checkinTime"
+                key="checkinTimeTime"
+                render={time => {
+                    return <span>{time && time.toLocaleTimeString("hu-HU")}</span>;
+                }}
+            />
+            <Column
+                title="Kijelentkezési idő"
+                dataIndex="checkoutTime"
+                key="checkoutTime"
+                render={time => {
+                    return <span>{time && time.toLocaleTimeString("hu-HU")}</span>;
+                }}
+            />
+            <Column
+                title="Megjegyzés"
+                dataIndex="comment"
+                key="comment"
+                render={comment => {
+                    return <Input value={comment}></Input>;
+                }}
+            />
+        </Table>
+    );
 };
--- src/components/Header.js
+++ src/components/Header.js
@@ -9,95 +9,95 @@ const { Option } = Select;
 const { dialog } = window.require("electron").remote;
 
 const DateComponent = () => {
-  const [date, setDate] = useState(new Date());
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(new Date());
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    const [date, setDate] = useState(new Date());
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(new Date());
+        }, 1000);
+        return () => clearInterval(interval);
+    }, []);
 
-  return <div style={{}}>{date.toLocaleString("hu-HU")}</div>;
+    return <div style={{}}>{date.toLocaleString("hu-HU")}</div>;
 };
 
 const dateFormatter = new Intl.DateTimeFormat("hu-HU", { month: "long" });
 
 const DateExporter = () => {
-  const [months, setMonths] = useState([]);
-  const { selectedMonth, setSelectedMonth } = useContext(SelectedDateContext);
+    const [months, setMonths] = useState([]);
+    const { selectedMonth, setSelectedMonth } = useContext(SelectedDateContext);
 
-  useEffect(() => {
-    const date = new Date();
-    const newMonths = [];
-    let year = date.getFullYear();
-    for (let i = year - 1; i < year + 1; i++) {
-      for (let j = 0; j < 12; j++) {
-        newMonths.push(new Date(i, j, 1));
-      }
-    }
-    setSelectedMonth(newMonths.find(m => m.getFullYear() === new Date().getFullYear() && m.getMonth() === new Date().getMonth()));
-    setMonths(newMonths);
-  }, [setSelectedMonth]);
+    useEffect(() => {
+        const date = new Date();
+        const newMonths = [];
+        let year = date.getFullYear();
+        for (let i = year - 1; i < year + 1; i++) {
+            for (let j = 0; j < 12; j++) {
+                newMonths.push(new Date(i, j, 1));
+            }
+        }
+        setSelectedMonth(newMonths.find(m => m.getFullYear() === new Date().getFullYear() && m.getMonth() === new Date().getMonth()));
+        setMonths(newMonths);
+    }, [setSelectedMonth]);
 
-  const onExportData = () => {
-    const fileName = dialog.showSaveDialogSync({ defaultPath: `${selectedMonth.getFullYear()}-${selectedMonth.getMonth() + 1}.csv` });
-    if (fileName) exportData(selectedMonth, fileName);
-  };
+    const onExportData = () => {
+        const fileName = dialog.showSaveDialogSync({ defaultPath: `${selectedMonth.getFullYear()}-${selectedMonth.getMonth() + 1}.csv` });
+        if (fileName) exportData(selectedMonth, fileName);
+    };
 
-  const handleChange = m => {
-    setSelectedMonth(m);
-  };
+    const handleChange = m => {
+        setSelectedMonth(new Date(m));
+    };
 
-  return (
-    <div style={{ display: "flex" }}>
-      <Select value={selectedMonth} style={{ width: 160 }} onChange={handleChange}>
-        {months.map(m => (
-          <Option value={m} key={`${m.year}.${m.month}`}>
-            {m.getFullYear()}. {dateFormatter.format(m)}
-          </Option>
-        ))}
-      </Select>
-      <Button style={{ marginLeft: 16 }} type="danger" onClick={onExportData}>
-        Havi adatok exportálása
-      </Button>
-    </div>
-  );
+    return (
+        <div style={{ display: "flex" }}>
+            <Select value={selectedMonth?.toString()} style={{ width: 160 }} onChange={handleChange}>
+                {months.map(m => (
+                    <Option value={m.toString()} key={`${m.getFullYear()}.${m.getMonth()}`}>
+                        {m.getFullYear()}. {dateFormatter.format(m)}
+                    </Option>
+                ))}
+            </Select>
+            <Button style={{ marginLeft: 16 }} type="danger" onClick={onExportData}>
+                Havi adatok exportálása
+            </Button>
+        </div>
+    );
 };
 
 export default ({ activeWindow, to }) => {
-  const [isOpen, setOpen] = useState(false);
+    const [isOpen, setOpen] = useState(false);
 
-  const onAdminClick = () => {
-    to("admin");
-    setOpen(false);
-  };
+    const onAdminClick = () => {
+        to("admin");
+        setOpen(false);
+    };
 
-  return (
-    <div
-      style={{
-        display: "flex",
-        marginBottom: 16,
-        justifyContent: "space-between",
-        alignItems: "center"
-      }}
-    >
-      <div style={{ display: "flex" }}>
-        {activeWindow === "normal" && (
-          <Button type="primary" onClick={() => setOpen(true)}>
-            Admin
-          </Button>
-        )}
-        {activeWindow === "admin" && (
-          <Button type="primary" onClick={() => to("normal")}>
-            Lista
-          </Button>
-        )}
-      </div>
+    return (
+        <div
+            style={{
+                display: "flex",
+                marginBottom: 16,
+                justifyContent: "space-between",
+                alignItems: "center"
+            }}
+        >
+            <div style={{ display: "flex" }}>
+                {activeWindow === "normal" && (
+                    <Button type="primary" onClick={() => setOpen(true)}>
+                        Admin
+                    </Button>
+                )}
+                {activeWindow === "admin" && (
+                    <Button type="primary" onClick={() => to("normal")}>
+                        Lista
+                    </Button>
+                )}
+            </div>
 
-      {activeWindow === "admin" && <DateExporter></DateExporter>}
+            {activeWindow === "admin" && <DateExporter></DateExporter>}
 
-      <PinPad isCheckout={false} pin={"2662"} onClose={() => setOpen(false)} isOpen={isOpen} onSave={onAdminClick}></PinPad>
-      <DateComponent></DateComponent>
-    </div>
-  );
+            <PinPad isCheckout={false} pin={"2662"} onClose={() => setOpen(false)} isOpen={isOpen} onSave={onAdminClick}></PinPad>
+            <DateComponent></DateComponent>
+        </div>
+    );
 };
